cea9e0be233dc399e1a2c14fa8a79150ac79900d
Brayan-SH <116335684+Brayan-SH@users.noreply.github.com>
2025-08-24 22:42:38 -0600

    avance 1

--- roles/Colaboradores/cajero.py
+++ roles/Colaboradores/cajero.py
@@ -1,8 +1,9 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-from pruebas import producto
 
-producto()
+# from pruebas import producto
 
 with open("productos.txt", "r") as file:
-    print(file.read())
+    row = file.readlines()
+    row = [line.strip().split(',') for line in row]
+    print(row)  # Acceder a las listas por indices.
