b98463b41f45543d5a5ab5e70c5ac70fe1db76ce
Brayan-SH <116335684+Brayan-SH@users.noreply.github.com>
2025-08-25 11:09:17 -0600

    actualizar

--- roles/Colaboradores/Cajeros/Funciones/generar_voucher.py
+++
@@ -1,8 +0,0 @@
-# Ticket de Venta
-# Fecha: 25/08/2025
-# --------------------------
-# Producto     Cantidad   Precio   Subtotal
-# Pan          2          10.00    20.00
-# Leche        1          15.00    15.00
-# --------------------------
-# TOTAL: 35.00
